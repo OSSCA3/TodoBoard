e4c4b16a2a2cbb3c935affa4f33173887d324f10
cl-o-lc <sujin2839@gmail.com>
2025-05-19 17:09:42 +0900

    chore: import문 관련 eslint 규칙 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import pluginTs from '@typescript-eslint/eslint-plugin';
 import parserTs from '@typescript-eslint/parser';
 import pluginReact from 'eslint-plugin-react';
 import pluginPrettier from 'eslint-plugin-prettier';
+import pluginImport from 'eslint-plugin-import';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,7 +17,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.{ts,tsx,js,tsx}'],
     languageOptions: {
       parser: parserTs,
       parserOptions: {
@@ -28,12 +29,58 @@ const eslintConfig = [
       '@typescript-eslint': pluginTs,
       react: pluginReact,
       prettier: pluginPrettier,
+      import: pluginImport,
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'no-unused-vars': 'off',
       'prettier/prettier': 'error',
+      'import/new-line-after-import': ['error', { count: 1 }],
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['../*', './*'],
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external', 'internal']],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'next',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@tanstack/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@supabase/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          'newlines-between': 'always',
+        },
+      ],
     },
     settings: {
       react: {
