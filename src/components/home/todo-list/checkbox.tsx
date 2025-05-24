e41c0fab4bf1f2fc4d4a17266b0690dc2d95ba2e
zelkovaria <hyein396@gmail.com>
2025-05-24 23:13:50 +0900

    feat: isCompleted에 따른 CheckBox 활성화

--- src/components/home/todo-list/checkbox.tsx
+++ src/components/home/todo-list/checkbox.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FaCheck } from 'react-icons/fa6';
+interface CheckBoxProps {
+  isCompleted: boolean;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+const CheckBox = ({ isCompleted, onClick }: CheckBoxProps) => {
+  const boxClasses =
+    'w-[22px] h-[22px] border border-[#65558f] rounded cursor-pointer flex justify-center items-center ' +
+    (isCompleted ? 'bg-[#65558f] text-white' : 'bg-transparent');
 
-const CheckBox = () => {
   return (
-    <div className="w-[22px] h-[22px] border border-[#65558f] rounded cursor-pointer flex justify-center items-center">
-      v
+    <div className={boxClasses} onClick={onClick}>
+      {isCompleted && <FaCheck className="text-white" />}
     </div>
   );
 };
--- src/components/home/todo-list/todo-item.tsx
+++ src/components/home/todo-list/todo-item.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { TodoItem } from '@/types/todo-list';
-import React from 'react';
+import React, { useState } from 'react';
 import { IoIosMore } from 'react-icons/io';
 import CheckBox from './checkbox';
 
@@ -7,10 +9,16 @@ interface TodoListProps {
   todo: TodoItem;
 }
 const TodoListItem = ({ todo }: TodoListProps) => {
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const handleIsCompleted = () => {
+    setIsCompleted((prev) => !prev);
+  };
+
   return (
     <li className="flex justify-between w-full border-b border-gray-200 last:border-b-0 py-2 select-none">
       <div className="flex items-center gap-3">
-        <CheckBox />
+        <CheckBox isCompleted={isCompleted} onClick={handleIsCompleted} />
         <div className="flex flex-col cursor-pointer truncate max-w-[340px]">
           <span>{todo.content}</span>
           <span>{todo.date}</span>
