86be58e98cbc33b51a0600903d070b16b04a6c90
zelkovaria <hyein396@gmail.com>
2025-05-24 21:55:00 +0900

    style: cursor pointer 및 icon 추가

--- src/components/home/bookmark/book-mark-card.tsx
+++ src/components/home/bookmark/book-mark-card.tsx
@@ -1,17 +1,19 @@
 import { BookmarkItem } from '@/types/bookmark';
 import React from 'react';
+import { IoIosMore } from 'react-icons/io';
 
 interface BookMarkCardProps {
   bookmark: BookmarkItem;
 }
 const BookMarkCard = ({ bookmark }: BookMarkCardProps) => {
   return (
-    <div className="min-w-[200px] max-w-[270px] border rounded min-h-28 max-h-32 flex gap-2 justify-between items-center p-6">
-      <div>
+    <div className="min-w-[200px] max-w-[270px] border rounded min-h-28 max-h-32 flex justify-between items-center p-6">
+      <div className="cursor-pointer">
         <div>{bookmark.title}</div>
         <div>{bookmark.description}</div>
       </div>
-      <div>,,,</div>
+
+      <IoIosMore className="text-xl cursor-pointer" />
     </div>
   );
 };
--- src/components/home/bookmark/book-mark.tsx
+++ src/components/home/bookmark/book-mark.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
 import { mockBookmarkData } from '@/mocks/dashboard';
 import BookMarkCard from './book-mark-card';
 
 const BookMark = () => {
   return (
     <div className="rounded-xl h-full">
-      <h2>북마크</h2>
+      <div className="flex justify-between items-center">
+        <h2>북마크</h2>
+        <FiPlus className="text-xl cursor-pointer" />
+      </div>
       <div className="grid justify-items-center grid-cols-2 gap-2">
         {mockBookmarkData.map((bookmark) => (
           <BookMarkCard bookmark={bookmark} />
