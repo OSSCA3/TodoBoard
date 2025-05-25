d51a2d98a391c316bb8c0efb1ad61162636dd430
dmdeh <131264106+dmdeh@users.noreply.github.com>
2025-05-25 23:00:19 +0900

    refactor: 코드리뷰 반영

--- src/components/home/dev-news/dev-news.tsx
+++ src/components/home/dev-news/dev-news.tsx
@@ -8,21 +8,21 @@ const DevNews = async () => {
       <div className="rounded-xl h-full w-full">
         <h2>개발 관련 소식</h2>
         <div className="space-y-4 overflow-y-auto flex-1 mt-4 max-h-[330px]">
-          {articles.map(({ title, description, tag_list }, index) => (
-            <div key={index} className="border-b pb-2">
+          {articles.map(({ title, description, tag_list }) => (
+            <article key={title} className="border-b pb-2">
               <h3 className="font-semibold">{title}</h3>
               <p className="text-sm text-gray-500">{description}</p>
               <div className="flex flex-wrap gap-2 mt-2">
-                {tag_list.map((tag, index) => (
+                {tag_list.map((tag) => (
                   <span
-                    key={index}
+                    key={tag}
                     className="text-xs bg-purple-100 px-2 py-1 rounded"
                   >
                     {tag}
                   </span>
                 ))}
               </div>
-            </div>
+            </article>
           ))}
         </div>
       </div>
