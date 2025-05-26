2f6e4a20f5da0824ece20ba99a7d51de0c3ba7e1
yoouyeon <jyeon.yoon59@gmail.com>
2025-05-26 10:37:04 +0900

    feat: 버튼 컴포넌트 추가
    - modal, alert, login에서 사용되는 기본 텍스트 버튼 컴포넌트
    - .gitkeep 파일 제거

---
+++ src/components/ui/buttons/button.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { ButtonHTMLAttributes } from 'react';
+
+const BUTTON_VARIANTS = {
+  primary: 'bg-[#8B5CF6] text-[#FFFEFE] hover:bg-[#7d53dd]',
+  secondary: 'bg-[#ECECEC] text-black hover:bg-[#D9D9D9]',
+  outline: 'border border-[#747775] text-[#1F1F1F] bg-transparent', // NOTE : google 로그인 버튼 디자인 참고 (https://developers.google.com/identity/branding-guidelines)
+} as const;
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: keyof typeof BUTTON_VARIANTS;
+}
+
+const Button = ({ variant = 'primary', className, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={`px-3 py-2.5 rounded-md text-sm font-medium cursor-pointer ${BUTTON_VARIANTS[variant]} ${className || ''}`}
+      {...props}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
