891173dba663d4c92016c761268c54b233521769
yoouyeon <jyeon.yoon59@gmail.com>
2025-05-26 22:36:34 +0900

    refactor: Input과 TextArea에 forwardRef 적용
    - 기존에는 타입만 정의되어 실제로 ref가 기대한대로 동작하지 않음
    - Input과 TextArea를 forwardRef로 변경하여 실제로 ref 전달이 가능하도록 함

--- src/components/ui/inputs/input.tsx
+++ src/components/ui/inputs/input.tsx
@@ -1,14 +1,18 @@
-import { ComponentPropsWithRef, InputHTMLAttributes } from 'react';
+import { ComponentPropsWithRef, forwardRef } from 'react';
 
 interface InputProps extends ComponentPropsWithRef<'input'> {}
 
-const Input = ({ className, ...props }: InputProps) => {
-  return (
-    <input
-      className={`w-full rounded-md border border-[#8B5CF6] px-3 py-2.5 text-black placeholder:text-[#989898] focus:outline-[#6f4ac5] focus-visible:outline-[#6f4ac5] ${className || ''}`}
-      {...props}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={`w-full rounded-md border border-[#8B5CF6] px-3 py-2.5 text-black placeholder:text-[#989898] focus:outline-[#6f4ac5] focus-visible:outline-[#6f4ac5] ${className || ''}`}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
 
+Input.displayName = 'Input';
 export default Input;
--- src/components/ui/inputs/text-area.tsx
+++ src/components/ui/inputs/text-area.tsx
@@ -1,60 +1,63 @@
 'use client';
 
-import { useState, ComponentPropsWithRef, ChangeEvent } from 'react';
+import {
+  forwardRef,
+  useState,
+  ComponentPropsWithRef,
+  ChangeEvent,
+} from 'react';
 
 interface TextAreaProps extends ComponentPropsWithRef<'textarea'> {
   maxLength?: number;
 }
 
-const TextArea = ({
-  className,
-  maxLength,
-  onChange,
-  value,
-  ...props
-}: TextAreaProps) => {
-  const [internalValue, setInternalValue] = useState(value || ''); // NOTE : 비제어 input에서도 글자수를 표시하기 위한 내부 상태
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, maxLength, onChange, value, ...props }, ref) => {
+    const [internalValue, setInternalValue] = useState(value || ''); // NOTE : 비제어 input에서도 글자수를 표시하기 위한 내부 상태
 
-  const isControlled = value !== undefined;
-  const isCheckMaxLength = maxLength !== undefined;
-  const currentLength = isControlled
-    ? value.toString().length
-    : internalValue.toString().length;
+    const isControlled = value !== undefined;
+    const isCheckMaxLength = maxLength !== undefined;
+    const currentLength = isControlled
+      ? value.toString().length
+      : internalValue.toString().length;
 
-  const changeValue = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    const newValue = event.target.value;
-    if (maxLength) {
-      // 글자수가 maxLength를 초과하지 않도록 제한
-      if (newValue.length > maxLength) {
-        event.target.value = newValue.slice(0, maxLength);
-        return; // 글자수가 초과하면 상태 업데이트를 하지 않음
+    const changeValue = (event: ChangeEvent<HTMLTextAreaElement>) => {
+      const newValue = event.target.value;
+      if (maxLength) {
+        // 글자수가 maxLength를 초과하지 않도록 제한
+        if (newValue.length > maxLength) {
+          event.target.value = newValue.slice(0, maxLength);
+          return; // 글자수가 초과하면 상태 업데이트를 하지 않음
+        }
       }
-    }
-    if (!isControlled) {
-      setInternalValue(newValue); // 비제어 컴포넌트인 경우 내부 상태 업데이트
-    }
-    if (onChange) {
-      onChange(event); // 부모 컴포넌트의 onChange 핸들러 호출
-    }
-  };
+      if (!isControlled) {
+        setInternalValue(newValue); // 비제어 컴포넌트인 경우 내부 상태 업데이트
+      }
+      if (onChange) {
+        onChange(event); // 부모 컴포넌트의 onChange 핸들러 호출
+      }
+    };
 
-  return (
-    <div
-      className={`w-full px-3 py-2.5 rounded-md border border-[#8B5CF6] ${className || ''}`}
-    >
-      <textarea
-        className="w-full px-1 text-black placeholder:text-[#989898] focus:outline-[#6f4ac5] focus-visible:outline-[#6f4ac5]"
-        onChange={changeValue}
-        value={isControlled ? value : undefined} // 제어 컴포넌트인 경우 부모 컴포넌트의 value 사용
-        {...props}
-      />
-      {isCheckMaxLength && (
-        <div className="mt-1 text-right text-xs text-[#989898]">
-          {currentLength}/{maxLength}
-        </div>
-      )}
-    </div>
-  );
-};
+    return (
+      <div
+        className={`w-full px-3 py-2.5 rounded-md border border-[#8B5CF6] ${className || ''}`}
+      >
+        <textarea
+          className="w-full px-1 text-black placeholder:text-[#989898] focus:outline-[#6f4ac5] focus-visible:outline-[#6f4ac5]"
+          onChange={changeValue}
+          value={isControlled ? value : undefined} // 제어 컴포넌트인 경우 부모 컴포넌트의 value 사용
+          ref={ref}
+          {...props}
+        />
+        {isCheckMaxLength && (
+          <div className="mt-1 text-right text-xs text-[#989898]">
+            {currentLength}/{maxLength}
+          </div>
+        )}
+      </div>
+    );
+  },
+);
 
+TextArea.displayName = 'TextArea';
 export default TextArea;
