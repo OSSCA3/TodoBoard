2e9f3ead6f1ad1a3c6fe2af440d8aad64d41afcb
yoouyeon <jyeon.yoon59@gmail.com>
2025-05-26 16:36:51 +0900

    feat: 입력 컴포넌트 추가
    - 입력을 위한 Input, TextArea 컴포넌트 생성
    - TextArea에서 최대 글자 수 제한 기능 추가

---
+++ src/components/ui/inputs/input.tsx
@@ -0,0 +1,14 @@
+import { ComponentPropsWithRef, InputHTMLAttributes } from 'react';
+
+interface InputProps extends ComponentPropsWithRef<'input'> {}
+
+const Input = ({ className, ...props }: InputProps) => {
+  return (
+    <input
+      className={`w-full rounded-md border border-[#8B5CF6] px-3 py-2.5 text-black placeholder:text-[#989898] focus:outline-[##6f4ac5] focus-visible:outline-[#6f4ac5] ${className || ''}`}
+      {...props}
+    />
+  );
+};
+
+export default Input;
