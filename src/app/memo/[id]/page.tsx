3f9b7d86ea5aee05d9166b9fc2f5be4424d4e2a0
yoouyeon <jyeon.yoon59@gmail.com>
2025-05-28 09:25:14 +0900

    feat: 메모 모달 추가
    - 메모 모달 구현
    - 사용하지 않는 모달 경로에서 리다이렉트하는 페이지 추가

---
+++ src/app/memo/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation';
+
+// NOTE : /memo/[id] 경로는 모달로만 사용하므로, url로 직접 접근하면 모달을 사용하는 페이지인 홈으로 리다이렉트합니다.
+const MemoPage = () => {
+  redirect('/');
+  return null;
+};
+
+export default MemoPage;
