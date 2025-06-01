97cb90487dafccd383d4e7948815dac365e70e31
zelkovaria <hyein396@gmail.com>
2025-06-02 01:11:06 +0900

    refactor: API 호출시 예외처리 추가

--- src/components/home/todo-list/todo-list.tsx
+++ src/components/home/todo-list/todo-list.tsx
@@ -5,25 +5,36 @@ import { TodoItem } from '@/types/todo-list';
 const TodoList = async () => {
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
 
-  const res = await fetch(`${baseUrl}/api/todos`, {});
-  if (!res.ok) console.error('할 일 목록 조회를 실패했어요');
-  const json = await res.json();
-  const todos: TodoItem[] = json.todos;
+  try {
+    const res = await fetch(`${baseUrl}/api/todos`, {});
+    if (!res.ok) console.error('할 일 목록 조회를 실패했어요');
+    const json = await res.json();
+    const todos: TodoItem[] = json.todos || [];
 
-  return (
-    <div className="rounded-xl h-full w-full">
-      <h2>할 일 리스트</h2>
-      <ul className="flex flex-col gap-2 max-h-[330px] overflow-y-auto">
-        {todos.length > 0 ? (
-          todos.map((todo) => <TodoListItem key={todo.id} todo={todo} />)
-        ) : (
-          <li className="text-gray-500 text-center py-4">
-            작성된 일정이 없습니다
-          </li>
-        )}
-      </ul>
-    </div>
-  );
+    return (
+      <div className="rounded-xl h-full w-full">
+        <h2>할 일 리스트</h2>
+        <ul className="flex flex-col gap-2 max-h-[330px] overflow-y-auto">
+          {todos.length > 0 ? (
+            todos.map((todo) => <TodoListItem key={todo.id} todo={todo} />)
+          ) : (
+            <li className="text-gray-500 text-center py-4">
+              작성된 일정이 없습니다
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className="rounded-xl h-full w-full">
+        <h2>할 일 리스트</h2>
+        <div className="flex flex-col gap-2 max-h-[330px]">
+          할 일 목록을 불러오는 중 오류가 발생했습니다
+        </div>
+      </div>
+    );
+  }
 };
 
 export default TodoList;
