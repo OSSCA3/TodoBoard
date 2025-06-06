ebd5cc4ea0ad9b7d409592624d099b4ab1f6eb28
yoouyeon <jyeon.yoon59@gmail.com>
2025-06-06 14:56:54 +0900

    feat: Supabase에 메모 저장 로직 추가

--- src/app/api/memo/add/route.ts
+++ src/app/api/memo/add/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { TABLE_NAME } from '@/constants/supabase';
 import { ErrorResponseBody } from '@/types/api';
+import { createAdminClient } from '@/libs/supabase/server';
 
 interface PostMemoResponseBody {
   title: string;
@@ -7,18 +9,33 @@ interface PostMemoResponseBody {
 }
 
 export async function POST(request: NextRequest) {
-  const { title, content } = await request.json();
+  try {
+    const { title, content } = await request.json();
 
-  if (!(title && content)) {
+    if (!(title && content)) {
+      return NextResponse.json<ErrorResponseBody>(
+        { message: '제목과 내용을 모두 입력해주세요' },
+        { status: 400 },
+      );
+    }
+
+    const supabase = await createAdminClient(); // TODO : 로그인 로직 이후에 인증된 사용자로 변경
+    await supabase.from(TABLE_NAME.MEMO).insert({
+      id: crypto.randomUUID(),
+      title,
+      content,
+    });
+
+    return NextResponse.json<PostMemoResponseBody>(
+      { title, content },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error('메모 추가 실패', error);
+
     return NextResponse.json<ErrorResponseBody>(
-      { message: '제목과 내용을 모두 입력해주세요' },
-      { status: 400 },
+      { message: '메모를 추가하는 중 오류가 발생했습니다.' },
+      { status: 500 },
     );
   }
-
-  // TODO : 메모를 db에 저장하는 로직 추가
-  return NextResponse.json<PostMemoResponseBody>(
-    { title, content },
-    { status: 201 },
-  );
 }
