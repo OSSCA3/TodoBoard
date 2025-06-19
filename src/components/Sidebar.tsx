fbe34345e6686348a0dd2d8b60cad1f046ff48e0
cl-o-lc <sujin2839@gmail.com>
2025-06-20 00:53:07 +0900

    feat: 네비게이션 버튼 구현

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+
+const navItems = [
+  { href: '/home', icon: '/icons/home.png', label: '홈' },
+  { href: '/todo', icon: '/icons/todo.png', label: '할 일' },
+  { href: '/user', icon: '/icons/setting.png', label: '사용자 설정' },
+];
+
+export default function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <aside className="fixed left-0 top-0 h-full w-16 bg-purple-200 flex flex-col items-center py-4 gap-6 z-50">
+      {/* 프로필 자리*/}
+      <div className="w-10 h-10 bg-white rounded-md" />
+
+      {/* 네비게이션 버튼 */}
+      {navItems.map((item) => (
+        <Link
+          key={item.href}
+          href={item.href}
+          className={`w-10 h-10 rounded-md flex items-center justify-center transition ${
+            pathname === item.href ? 'bg-purple-400' : 'hover:bg-purple-300'
+          }`}
+        >
+          <Image src={item.icon} alt={item.label} width={24} height={24} />
+        </Link>
+      ))}
+    </aside>
+  );
+}
